50c90ca673e49432d4ae400a93ef9595a6212fc3
hscheuerle <h.a.scheuerle@gmail.com>
2021-01-19 17:12:36 -0500

    add ios and env

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -17,6 +17,10 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <h1>
+          {process.env.NODE_ENV}
+          {process.env.REACT_APP_TESTVALUE}
+        </h1>
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
